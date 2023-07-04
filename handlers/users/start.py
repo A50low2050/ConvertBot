089107e29cad78e6696ae9b2c0719f6ae91e1354
Artem <artem.alshan@yandex.ru>
2023-07-04 13:26:05 +0300

    Add welcome text

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,7 +7,9 @@ from create_bot import bot
 async def start(msg: Message):
     chat_id = msg.chat.id
     first_name = msg.from_user.first_name
-    await bot.send_message(chat_id, f'Привет {first_name}')
+    await bot.send_message(chat_id, f'Hi {first_name}🖐\n'
+                                    f'To convert a video to MP3 format, download a video with MP4 resolution.\n\n'
+                                    f'📌 Bot can download files of up to 20MB in size <a href="https://core.telegram.org/bots/api#getfile">limit Telegram</a>.')
 
 
 def register_handler_start(dp: Dispatcher):
