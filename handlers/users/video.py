2920185f4a25ccd2b2f34a2de6c0d00236464f26
Artem <artem.alshan@yandex.ru>
2023-07-11 13:09:58 +0300

    Change structure code download video and transformed him

--- handlers/users/video.py
+++ handlers/users/video.py
@@ -1,8 +1,10 @@
+import os.path
+
 from aiogram import Dispatcher
-from aiogram.utils.exceptions import FileIsTooBig
+from aiogram.utils.exceptions import FileIsTooBig, NotFound
 from aiogram.types import Message
 from create_bot import bot
-from config import TOKEN, PATH, MEDIA
+from config import VIDEOS, PATH, MEDIA
 from utils.processor import converter_to_mp3
 from utils.handler_title import convert_video_title
 from moviepy.editor import *
@@ -14,18 +16,34 @@ async def convert_audio(msg: Message):
 
     try:
         await bot.send_message(chat_id, 'Loading ⏳')
-        file = await bot.get_file(video_id)
-        download_url = f"https://api.telegram.org/file/bot{TOKEN}/{file.file_path}"
         video_name = msg.video.file_name
 
-        converter_to_mp3(download_url, video_name)
-        audio_name = convert_video_title(video_name)
+        if not os.path.exists(os.path.join(PATH, MEDIA, video_name, VIDEOS)):
+            video = await msg.video.download(destination_dir=os.path.join(PATH, MEDIA, video_name))
 
-        audio_path = os.path.join(PATH, MEDIA, audio_name)
+            video_dir = os.path.join(PATH, MEDIA, video_name, VIDEOS)
 
-        with open(audio_path, mode='rb') as file:
-            audio = file.read()
-            await bot.send_audio(chat_id=chat_id, audio=audio, title=audio_name.split('.')[0])
+            save_video = []
+            for file in os.listdir(video_dir):
+                save_video.append(file)
+
+            converter_to_mp3(str(os.path.join(video_dir, save_video[0])), video_name)
+            audio_name = convert_video_title(video_name)
+
+            audio_path = os.path.join(PATH, MEDIA, video_name, VIDEOS, audio_name)
+
+            with open(audio_path, mode='rb') as file:
+                audio = file.read()
+                await bot.send_audio(chat_id=chat_id, audio=audio, title=audio_name.split('.')[0])
+                file.close()
+        else:
+            for file in os.listdir(os.path.join(PATH, MEDIA, video_name, VIDEOS)):
+                if file.endswith(".mp3"):
+                    audio_title = file.split('.')[0]
+                    with open(os.path.join(PATH, MEDIA, video_name, VIDEOS, file), mode='rb') as file:
+                        audio = file.read()
+                        await bot.send_audio(chat_id=chat_id, audio=audio, title=audio_title)
+                        file.close()
 
     except FileIsTooBig as ex:
         print(ex)
