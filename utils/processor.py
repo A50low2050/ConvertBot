05b8786a945ad1c5f1250c157c5a085e80b073f4
Artem <artem.alshan@yandex.ru>
2023-07-11 13:08:28 +0300

    Add new path save audio

--- utils/processor.py
+++ utils/processor.py
@@ -1,5 +1,5 @@
 from moviepy.editor import *
-from config import PATH, MEDIA
+from config import VIDEOS, PATH, MEDIA
 from utils.handler_title import convert_video_title
 
 
@@ -7,6 +7,6 @@ def converter_to_mp3(url_video: str, video_title: str):
 
     video = VideoFileClip(url_video)
     video_name = convert_video_title(video_title)
-    video.audio.write_audiofile(os.path.join(PATH, MEDIA, video_name))
+    video.audio.write_audiofile(os.path.join(PATH, MEDIA, video_title, VIDEOS, video_name))
 
     return video
