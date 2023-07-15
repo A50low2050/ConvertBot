1afca8def1a8b2199f1e5b14469617abe9ea695d
Artem <artem.alshan@yandex.ru>
2023-07-16 01:13:47 +0300

    exception handler created

--- utils/clear_media.py
+++ utils/clear_media.py
@@ -4,10 +4,16 @@ from config import PATH, MEDIA
 
 
 def clear_media():
-    for address, dirs, files in os.walk(os.path.join(PATH, MEDIA)):
-        for name in files:
-            os.remove(os.path.join(address, name))
+    try:
+        for address, dirs, files in os.walk(os.path.join(PATH, MEDIA)):
+            for name in files:
+                os.remove(os.path.join(address, name))
 
-    for dirs in os.listdir(os.path.join(PATH, MEDIA)):
-        shutil.rmtree(os.path.join(PATH, MEDIA, dirs))
+        for dirs in os.listdir(os.path.join(PATH, MEDIA)):
+            shutil.rmtree(os.path.join(PATH, MEDIA, dirs))
+    except PermissionError as error:
+        print(error)
+        return '[WinError 32] Процесс не может получить доступ к файлу, так как этот файл занят другим процессом ❗'
+
+    return 'Clear folder is success ✅'
 
