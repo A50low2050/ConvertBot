0ec19bf222d681c79f1bf840502bd1916058efe8
Artem <artem.alshan@yandex.ru>
2023-07-16 00:03:59 +0300

    Add clear media

--- utils/clear_media.py
+++ utils/clear_media.py
@@ -1,6 +1,13 @@
 import os
+import shutil
 from config import PATH, MEDIA
 
-for address, dirs, files in os.walk(os.path.join(PATH, MEDIA)):
-    for name in files:
-        os.remove(os.path.join(address, name))
+
+def clear_media():
+    for address, dirs, files in os.walk(os.path.join(PATH, MEDIA)):
+        for name in files:
+            os.remove(os.path.join(address, name))
+
+    for dirs in os.listdir(os.path.join(PATH, MEDIA)):
+        shutil.rmtree(os.path.join(PATH, MEDIA, dirs))
+
